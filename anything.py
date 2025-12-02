34328a63aa949a93dc9729afe572bb52664a5438
youngseo <goindol1801@naver.com>
2025-12-02 15:37:25 +0900

    merge conflict

--- anything.py
+++ anything.py
@@ -1,3 +1,6 @@
+
+     from add_lists import add_lists
+
 if __name__ == "__main__":
-    print("hello python, hi ros.")
-     print("Python is the best language for ros")
+    result = add_lists(['wel', 't', 'r'], ['come', 'o', 'os'])
+    print(result)
