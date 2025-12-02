5a824b7d7e2054babcea4169668bfb28a117d686
youngseo <goindol1801@naver.com>
2025-12-02 16:05:23 +0900

    edit main.py

---
+++ somedir/dict_ops.py
@@ -0,0 +1,28 @@
+# dict_ops.py
+def add(foo, bar):
+    out = {}
+    for key in foo.keys():
+        if key in bar:
+            out[key] = foo[key] + bar[key]
+    return out
+
+def subtract(foo, bar):
+    out = {}
+    for key in foo.keys():
+        if key in bar:
+            out[key] = foo[key] - bar[key]
+    return out
+
+def multiply(foo, bar):
+    out = {}
+    for key in foo.keys():
+        if key in bar:
+            out[key] = foo[key] * bar[key]
+    return out
+
+def divide(foo, bar):
+    out = {}
+    for key in foo.keys():
+        if key in bar:
+            out[key] = foo[key] / bar[key]
+    return out
