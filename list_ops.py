7adc69cf37bfe1ed5e324f1d4f99b13fb1c592a6
youngseo <goindol1801@naver.com>
2025-12-15 00:48:43 +0900

    implement multiply and substract

--- list_ops.py
+++ list_ops.py
@@ -10,3 +10,19 @@ def subtract(foo, bar):
     for f, b in zip(foo, bar):
         out.append(f - b)
     return out
+    
+    
+    
+    
+    # 아래쪽에 추가
+def multiply(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f * b)
+    return out
+
+def divide(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f / b)
+    return out
