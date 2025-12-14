a6281865db43baa53ebf9c1fac804f10d02d6ce3
youngseo <goindol1801@naver.com>
2025-12-15 00:47:19 +0900

    implement add and subtract

---
+++ list_ops.py
@@ -0,0 +1,12 @@
+# list_ops.py
+def add(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f + b)
+    return out
+
+def subtract(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f - b)
+    return out
