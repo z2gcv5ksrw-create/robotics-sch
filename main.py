abab32f93322309c74ccc25c77b2aa1bf2ac30de
youngseo <goindol1801@naver.com>
2025-12-15 00:49:33 +0900

    change main to use list ops

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+import list_ops as lo
+
+if __name__ == "__main__":
+    foo = [1,2,3]
+    bar = [4,5,6]
+    print("foo+bar=", lo.add(foo, bar))
+    print("foo-bar=", lo.subtract(foo, bar))
+    print("foo*bar=", lo.multiply(foo, bar))
+    print("foo/bar=", lo.divide(foo, bar))
